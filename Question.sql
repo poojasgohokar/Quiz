a5a223f417206c0a8879f235d2ef242470583f04
poojasgohokar <72243440+poojasgohokar@users.noreply.github.com>
2023-01-15 19:41:00 +0530

    Add files via upload

---
+++ Question.sql
@@ -0,0 +1,18 @@
+create table quiz(
+Questions varchar(500),
+OptionA varchar(200),
+OptionB varchar(200),
+OptionC varchar(200),
+OptionD varchar(200)
+);
+
+insert into quiz (Questions,OptionA,OptionB,OptionC,OptionD) values ("Who invented Java Programming?","James Gosling","Guido van Rossum","Dennis Ritchie","Bjarne Stroustrup");
+insert into quiz (Questions,OptionA,OptionB,OptionC,OptionD) values ("Which component is used to compile, debug and execute the java programs?","JRE","JIT","JDK","JVM");
+insert into quiz (Questions,OptionA,OptionB,OptionC,OptionD) values ("Which of these cannot be used for a variable name in Java?","identifier & keyword","identifier","keyword"," none of the mentioned");
+insert into quiz (Questions,OptionA,OptionB,OptionC,OptionD) values ("Which environment variable is used to set the java path?","MAVEN_Path","JavaPATH","JAVA","JAVA_HOME");
+insert into quiz (Questions,OptionA,OptionB,OptionC,OptionD) values ("Which of the following is not an OOPS concept in Java?","Polymorphism","Inheritance","Compilation","Encapsulation");
+insert into quiz (Questions,OptionA,OptionB,OptionC,OptionD) values ("What is the extension of compiled java classes?",".txt",".js",".class",".java");
+insert into quiz (Questions,OptionA,OptionB,OptionC,OptionD) values ("Array in Java are-","Object References","Object","Primitive DataType","None");
+insert into quiz (Questions,OptionA,OptionB,OptionC,OptionD) values (" Which of these keywords is used to define interfaces in Java?","intf","Intf","interface","Interface");
+insert into quiz (Questions,OptionA,OptionB,OptionC,OptionD) values ("Which one of the following is not an access modifier?","Protected","Void","Public","Private");
+insert into quiz (Questions,OptionA,OptionB,OptionC,OptionD) values ("What is the initial quantity of the ArrayList list?","5","10","0","100");
---
+++ student1.sql
@@ -0,0 +1,5 @@
+create table student1(
+StudentId Int(200) primary key auto_increment,
+StudentName varchar(500),
+Marks int(100)
+);
